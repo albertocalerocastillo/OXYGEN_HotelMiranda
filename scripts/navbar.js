7ff3599ead48d4ee4a19cc1e363ebcc568ae270d
albertocalerocastillo <albertocalerocastillo@gmail.com>
2025-01-07 15:43:01 +0100

    ADD: Footer and Rooms Page

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,11 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuImg = document.getElementById("menu-img");
     const desplegable = document.getElementById("desplegable");
 
+    const basePath = window.location.pathname.includes("rooms") ? "../" : "./";
+
     menuIcono.addEventListener("click", () => {
         desplegable.classList.toggle("activo");
+
         menuImg.src = desplegable.classList.contains("activo")
-            ? "./images/close.png"
-            : "./images/nav.png";
+            ? `${basePath}images/close.png`
+            : `${basePath}images/nav.png`;
+
         document.querySelector(".navbar").classList.toggle("navbar-desplegado");
     });
 });
